7a517ef59548ad6d19447f40dc507798992fa4b0
ree-rishun <42071202+ree-rishun@users.noreply.github.com>
2019-10-20 07:44:45 +0900

    update

--- RPi/emove_player.py
+++ RPi/emove_player.py
@@ -8,20 +8,32 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 
-# firebase
+# firebase config
 cred = credentials.Certificate("./serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 
 # define
+# アウトロ時の被せ時間
 OUTRO_TIME = 1
+
+# mp3ファイル格納ディレクトリ
 MUSIC_PATH = "/jphacks/music/"
 
+# mp3予備ファイルパス
+
+# mp3予備ファイル時間
+
+
+# 楽曲再生関数
 def sound_play(path):
 	os.system("mpg321 " + path + " &")
 
+
+# 楽曲停止関数
 def sound_stop():
 	os.system("killall mpg321")
+
 
 # main
 def main():
@@ -34,26 +46,27 @@ def main():
 	for doc_c in docs_c:
 		data_c = doc_c.to_dict()
 		print(format(data_c['musicID']))
-		# 楽曲情報の更新
-		# prev_m_path=next_m_path
-		# prev_m_time=next_m_path
 
 		# 楽曲情報の取得
 		docs_m = db.collection('music').get()
 		for doc_m in docs_m:
 			data_m = doc_m.to_dict()
-			if format(data_c['musicID']) == format(data_m['musicID']):
-				next_m_path=format(data_m['path'])
-				next_m_time=format(data_m['time'])
 
-		print(next_m_path)
-		print(next_m_time)
+			# IDから楽曲情報を取得
+			if format(data_c['musicID']) == format(data_m['musicID']):
+
+				# mp3ファイルの有無を確認
+				if os.path.isfile(MUSIC_PATH + format(data_m['path'])):
+					next_m_path=format(data_m['path'])
+					next_m_time=format(data_m['time'])
+				else:
+					print("ERROR: Not found " + MUSIC_PATH + format(data_m['path']))
 
 		# 楽曲の再生
-		sound_play(MUSIC_PATH+next_m_path)
+		sound_play(MUSIC_PATH + next_m_path)
 
 		# 楽曲の再生中待機
-		time.sleep(int(next_m_time)-OUTRO_TIME)
+		time.sleep(int(next_m_time) - OUTRO_TIME)
 
 
 if __name__ == "__main__":
