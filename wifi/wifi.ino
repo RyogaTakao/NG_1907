05ab6b9ef093058222c42825da8bf627e4da4f18
oishimilk <56726852+oishimilk@users.noreply.github.com>
2019-10-19 13:53:12 +0900

    とりあえず動いたので上げます

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,8 +1,20 @@
 #include <M5Stack.h>
 #include <WiFi.h>
-#include <ESPmDNS.h>
 #include <WiFiClient.h>
-#include <Preferences.h>
+
+//各種設定
+//セキュリティもクソもないです。
+#define SSID "NG1907"
+#define PASSWORD "Shikishima"
+#define SRV_IP_ADDR "192.168.137.42"
+#define SRV_PORT 12345
+#define CONNECTION_WAIT 1000
+
+#define BAUD_RATE 115200
+
+//みんな大好きグローバル変数
+//接頭辞 glb_ をつけてください。
+WiFiClient glb_wifi_client;
 
 /*
   M5Stackの各種セットアップを行います。
@@ -13,12 +25,12 @@
 void setup() {
   M5.begin();
 
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
   delay(500);
   Serial.printf("\nM5 ready!\n");
 
-  connect();
-  post();
+  connectAP();
+  connectTCP();
 }
 
 /*
@@ -27,21 +39,21 @@ void setup() {
   @param なし [なし]: この関数に引数はありません。
   @return N/A [bool]: 接続が成功したか否か。
 */
-bool connect() {
+bool connectAP(void) {
   //設定
-  const char ssid[] = "NG1907";
-  const char password[] = "Shikishima";
   unsigned int patience = 50;
 
+  Serial.printf("Trying to connect to AP...\n");
+
   //接続
-  WiFi.begin(ssid, password);
+  WiFi.begin(SSID, PASSWORD);
   while (patience--) {
     if (WiFi.status() == WL_CONNECTED) {
       Serial.printf("Connected!\n");
       return true;
     } else {
       Serial.printf(".");
-      delay(500);
+      delay(CONNECTION_WAIT);
     }
   }
 
@@ -50,11 +62,32 @@ bool connect() {
 }
 
 /*
-  
-*/
-void post() {
+  TCP接続を確立します。
 
+  @param なし [なし]: この関数に引数はありません。
+  @return N/A [bool]: 接続が成功したか否か。
+*/
+bool connectTCP(void){
+  //設定
+  unsigned int patience = 50;
+
+  Serial.printf("Trying to connect to server...\n");
+  while (patience--) {
+    if (glb_wifi_client.connect(SRV_IP_ADDR, SRV_PORT)) {
+      Serial.printf("TCP connection established.\n");
+      return true;
+    } else {
+      Serial.printf(".");
+      delay(CONNECTION_WAIT);
+    }
+  }
+
+  Serial.printf("Connection timed out.\n");
+  return false;
 }
 
 void loop() {
+  static unsigned int count = 0;
+  glb_wifi_client.printf("%d\n", count++);
+  delay(1000);
 }
