9c08fd5ef7d2912cbd1e57429afdf02322b63644
oishimilk <56726852+oishimilk@users.noreply.github.com>
2019-10-19 19:16:08 +0900

    ノイズ対策強化

--- m5stack/m5stack.ino
+++ m5stack/m5stack.ino
@@ -20,14 +20,13 @@
 
 //FFT設定
 //FFT_SAMPLES は 2^n でなければなりません。
-#define FFT_SAMPLES 16
-#define FFT_SAMPLING_FREQUENCY 4
-
-arduinoFFT FFT = arduinoFFT();
+#define FFT_SAMPLES 32
+#define FFT_SAMPLING_FREQUENCY 8
 
 //みんな大好きグローバル変数
 //接頭辞 glb_ をつけてください。
 WiFiClient glb_wifi_client;
+arduinoFFT glb_fft;
 
 /*
   M5Stackの各種セットアップを行います。
@@ -48,6 +47,9 @@ void setup() {
 
   //心拍計
   pinMode(PIN_INPUT, INPUT);
+
+  //FFT
+  glb_fft = arduinoFFT();
 
   //Wi-Fi
   //connectAP();
@@ -109,33 +111,48 @@ bool connectTCP(void){
   return false;
 }
 
+/*
+  メインの繰り返し処理です。
+  PPDR (https://ppdr.softether.net/esp-fft-test) を基に改変しています。
+
+  @param なし [なし]: この関数に引数はありません。
+  @return なし [なし]: この関数は戻り値がありません。
+*/
 void loop() {
   //1サンプルの間隔
-  const unsigned int sampling_period_us = round(1000000*(1.0/FFT_SAMPLING_FREQUENCY));
+  const unsigned int sampling_period_us = round(1000000.0/FFT_SAMPLING_FREQUENCY);
 
   //波形データ格納用
   double real[FFT_SAMPLES], imaginary[FFT_SAMPLES];
 
+  //bpm
+  static unsigned int bpm = 120;
+
   //サンプリング
   for (unsigned int i=0; i < FFT_SAMPLES; i++) {
-    unsigned long microseconds = micros();
+    unsigned long before = micros();
 
     real[i] = analogRead(PIN_INPUT);
     imaginary[i] = 0;
 
-    while(micros() < (microseconds + sampling_period_us));
+    while(micros() < (before + sampling_period_us));
   }
 
-  //FFT
-  FFT.Windowing(real, FFT_SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
-  FFT.Compute(real, imaginary, FFT_SAMPLES, FFT_FORWARD);
-  FFT.ComplexToMagnitude(real, imaginary, FFT_SAMPLES);
+  //高速フーリエ変換を行い、時系列情報を周波数情報に変換します。
+  glb_fft.Windowing(real, FFT_SAMPLES, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  glb_fft.Compute(real, imaginary, FFT_SAMPLES, FFT_FORWARD);
+  glb_fft.ComplexToMagnitude(real, imaginary, FFT_SAMPLES);
 
   //最もエネルギーの大きい周波数を取り出す(Hz)
-  double peak = FFT.MajorPeak(real, FFT_SAMPLES, FFT_SAMPLING_FREQUENCY);
+  double peak = glb_fft.MajorPeak(real, FFT_SAMPLES, FFT_SAMPLING_FREQUENCY);
 
   //BPMに変換
-  unsigned int bpm = peak * 60;
+  //前のBPMから突然2倍以上になった場合はノイズとして判断します
+  if (peak * 60 > bpm * 2) {
+    bpm = peak * 60 / 2;
+  } else {
+    bpm = peak * 60;
+  }
 
   //画面
   M5.Lcd.setCursor(1, 1);
